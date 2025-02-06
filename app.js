720c6957938381b6ec133554a472481f9eea98fd
JashawnRogers <rogersjashawn20@gmail.com>
2025-02-05 17:55:38 -0700

    Added dynamic time functionality and removed unused files

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const time = document.getElementById('time');
 const date = document.getElementById('date');
 const btn = document.getElementById('tzModal');
 const modal = document.getElementById('modal-1');
+const modalCloseBtn = document.getElementById('closeBtn');
+const timezoneList = document.getElementById('tzList');
 
 MicroModal.init();
 
 timezone.innerText = `${dayjs.tz.guess()}`;
-time.innerText = `${dayjs().format("h:mm:ss A")}`;
-date.innerText = `${dayjs().format('dddd, MMMM D')}`
+date.innerText = `${dayjs().format('dddd, MMMM D')}`;
+
+// Get interval ID so that is can be cleared when user chooses timezone
+let intervalID = setInterval(() => time.innerText = `${dayjs().format("h:mm:ss A")}`, 1000);
+
 
 btn.addEventListener('click', () => {
     if (modal.classList === 'modal') {
@@ -16,4 +21,11 @@ btn.addEventListener('click', () => {
     } else {
         MicroModal.show('modal-1');
     }
+})
+
+modalCloseBtn.addEventListener('click', () => {
+    clearInterval(intervalID);
+   const userTimezone = timezoneList.value;
+   timezone.innerText = `${timezoneList.options[timezoneList.selectedIndex].text}`;
+   setInterval(() => time.innerText = `${dayjs().tz(userTimezone).format("h:mm:ss A")}`, 1000);
 })
